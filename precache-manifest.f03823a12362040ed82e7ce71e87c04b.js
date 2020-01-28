b9f8e700edeef2ef774473cb2c4644b042739b57
rueyho <ruey.ho@simplr.com.my>
2020-01-28 11:17:05 +0800

    Updates

---
+++ precache-manifest.f03823a12362040ed82e7ce71e87c04b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f763c5645665e9d700ff796c9dc1e6",
+    "url": "/testingReact/index.html"
+  },
+  {
+    "revision": "463b97084650d759fd8e",
+    "url": "/testingReact/static/css/9.cc0e3a15.chunk.css"
+  },
+  {
+    "revision": "3c2c1b80983d5fad9c9e",
+    "url": "/testingReact/static/css/main.492d099e.chunk.css"
+  },
+  {
+    "revision": "8e365db11ca4a3ae6f62",
+    "url": "/testingReact/static/js/0.82ae671f.chunk.js"
+  },
+  {
+    "revision": "96ae99fe1e978b43420e",
+    "url": "/testingReact/static/js/1.e543e81a.chunk.js"
+  },
+  {
+    "revision": "28fe77ad013cd2f9b530",
+    "url": "/testingReact/static/js/10.ec5ba8d5.chunk.js"
+  },
+  {
+    "revision": "e5ec60a177add6c3f0fa",
+    "url": "/testingReact/static/js/11.1fd5d04a.chunk.js"
+  },
+  {
+    "revision": "77002759c5f2eea14b63",
+    "url": "/testingReact/static/js/12.3b2b0ca2.chunk.js"
+  },
+  {
+    "revision": "2d16eae61e948bdbf474",
+    "url": "/testingReact/static/js/13.69b095b7.chunk.js"
+  },
+  {
+    "revision": "1b5dda54d16c6794982b",
+    "url": "/testingReact/static/js/14.646b3e85.chunk.js"
+  },
+  {
+    "revision": "6c932b4a130f2ab6b1bb",
+    "url": "/testingReact/static/js/2.df3c8797.chunk.js"
+  },
+  {
+    "revision": "210a126c62c8da583d67",
+    "url": "/testingReact/static/js/3.44ae9325.chunk.js"
+  },
+  {
+    "revision": "dcf46bc0687569f1a425",
+    "url": "/testingReact/static/js/6.ffaa2d50.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/testingReact/static/js/6.ffaa2d50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1673cabc7a748f17d99e",
+    "url": "/testingReact/static/js/7.cb941169.chunk.js"
+  },
+  {
+    "revision": "a78c59afde9dcef76dff",
+    "url": "/testingReact/static/js/8.4f436939.chunk.js"
+  },
+  {
+    "revision": "463b97084650d759fd8e",
+    "url": "/testingReact/static/js/9.baccbdc1.chunk.js"
+  },
+  {
+    "revision": "3c2c1b80983d5fad9c9e",
+    "url": "/testingReact/static/js/main.d294a31d.chunk.js"
+  },
+  {
+    "revision": "bb8600e6c620e7537097",
+    "url": "/testingReact/static/js/runtime-main.de803f7a.js"
+  },
+  {
+    "revision": "e2d82b53a65f4a09a9e403afe821ab07",
+    "url": "/testingReact/static/media/bg-login.e2d82b53.jpeg"
+  }
+]);
